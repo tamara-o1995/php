a9814d948203f6563047d167bc34d1fc23532801
tamara-o1995 <omelaniukt@gmail.com>
2021-03-19 15:51:17 -0300

    Cambios

---
+++ listado_productos.php
@@ -0,0 +1,59 @@
+<?php
+$aProductos = array();
+$aProductos[] = array("nombre" => "Smart TV 55\" 4K UHD",
+                   "marca" => "Hitachi",
+                   "modelo" => "554KS20",
+                   "stock" => 60,
+                   "precio" => 58000
+);
+$aProductos[] = array("nombre" => "Samsung Galaxy A30 Blanco",
+                   "marca" => "Samsung",
+                   "modelo" => "Galaxy A30",
+                   "stock" => 0,
+                   "precio" => 22000
+);
+$aProductos[] = array("nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
+                   "marca" => "Surrey",
+                   "modelo" => "553AIQ1201E",
+                   "stock" => 5,
+                   "precio" => 45000
+);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Productos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12 pt-3 pb-5 text-center">
+            <h1>Listado de productos</h1>
+        </div>
+    </div>
+</div>
+
+<table class="table table-hover border">
+    <tr>
+        <th>Nombre</th>
+        <th>marca</th>
+        <th>modelo</th>
+        <th>stock</th>
+        <th>Precio</th>
+        <th>Acción</th>
+    </tr>
+
+<tr>
+
+
+</tr>
+
+</table>
+</body>
+</html>
+
---
+++ rand.php
@@ -0,0 +1,10 @@
+<?php
+$valor= rand (1,5);
+if ($valor== 1 || $valor == 3 || $valor == 5){
+    echo "el numero $valor es impar";
+}
+else{
+    echo "el numero $valor es par";
+}
+
+?>
