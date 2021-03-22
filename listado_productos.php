eaa4a0c3f2f7111a6709318b15b1cf7dc9927f2a
tamara-o1995 <omelaniukt@gmail.com>
2021-03-22 19:16:41 -0300

    listado_productos & arreglos en ficha-personal

--- listado_productos.php
+++ listado_productos.php
@@ -1,27 +1,31 @@
 <?php
 $aProductos = array();
-$aProductos[] = array("nombre" => "Smart TV 55\" 4K UHD",
-                   "marca" => "Hitachi",
-                   "modelo" => "554KS20",
-                   "stock" => 60,
-                   "precio" => 58000
+$aProductos[0] = array(
+    "nombre" => "Smart TV 55\" 4K UHD",
+    "marca" => "Hitachi",
+    "modelo" => "554KS20",
+    "stock" => 60,
+    "precio" => 58000
 );
-$aProductos[] = array("nombre" => "Samsung Galaxy A30 Blanco",
-                   "marca" => "Samsung",
-                   "modelo" => "Galaxy A30",
-                   "stock" => 0,
-                   "precio" => 22000
+$aProductos[1] = array(
+    "nombre" => "Samsung Galaxy A30 Blanco",
+    "marca" => "Samsung",
+    "modelo" => "Galaxy A30",
+    "stock" => 0,
+    "precio" => 22000
 );
-$aProductos[] = array("nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
-                   "marca" => "Surrey",
-                   "modelo" => "553AIQ1201E",
-                   "stock" => 5,
-                   "precio" => 45000
+$aProductos[2] = array(
+    "nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
+    "marca" => "Surrey",
+    "modelo" => "553AIQ1201E",
+    "stock" => 5,
+    "precio" => 45000
 );
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,31 +33,51 @@ $aProductos[] = array("nombre" => "Aire Acondicionado Split Inverter Frío/Calor
     <title>Listado de Productos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
+
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-12 pt-3 pb-5 text-center">
-            <h1>Listado de productos</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 text-center mb-4">
+                <h1>Listado de productos</h1>
+            </div>
         </div>
+        <table class="table table-hover border">
+            <tr>
+                <th>Nombre</th>
+                <th>Marca</th>
+                <th>Modelo</th>
+                <th>Stock</th>
+                <th>Precio</th>
+                <th>Acción</th>
+            </tr>
+
+            <tr>
+                <td><?php echo $aProductos[0]["nombre"]; ?></td>
+                <td><?php echo $aProductos[0]["marca"]; ?></td>
+                <td><?php echo $aProductos[0]["modelo"]; ?></td>
+                <td><?php echo $aProductos[0]["stock"] == 0 ? "Sin stock" : ($aProductos[0]["stock"] >= 1 && $aProductos[0]["stock"] <= 10 ? "Poco stock" : "Hay stock") ?></td>
+                <td><?php echo $aProductos[0]["precio"]; ?></td>
+                <td><button class="btn btn-primary">Comprar</button></td>
+            </tr>
+            <tr>
+                <td><?php echo $aProductos[1]["nombre"]; ?></td>
+                <td><?php echo $aProductos[1]["marca"]; ?></td>
+                <td><?php echo $aProductos[1]["modelo"]; ?></td>
+                <td><?php echo $aProductos[1]["stock"] == 0 ? "Sin stock" : ($aProductos[1]["stock"] >= 1 && $aProductos[1]["stock"] <= 10 ? "Poco stock" : "Hay stock") ?></td>
+                <td><?php echo $aProductos[1]["precio"]; ?></td>
+                <td><button class="btn btn-primary">Comprar</button></td>
+            </tr>
+            <tr>
+                <td><?php echo $aProductos[2]["nombre"]; ?></td>
+                <td><?php echo $aProductos[2]["marca"]; ?></td>
+                <td><?php echo $aProductos[2]["modelo"]; ?></td>
+                <td><?php echo $aProductos[2]["stock"] == 0 ? "Sin stock" : ($aProductos[2]["stock"] >= 1 && $aProductos[2]["stock"] <= 10 ? "Poco stock" : "Hay stock") ?></td>
+                <td><?php echo $aProductos[2]["precio"]; ?></td>
+                <td><button class="btn btn-primary">Comprar</button></td>
+            </tr>
+        </table>
     </div>
-</div>
-<div></div>
-<table class="table table-hover border">
-    <tr>
-        <th>Nombre</th>
-        <th>marca</th>
-        <th>modelo</th>
-        <th>stock</th>
-        <th>Precio</th>
-        <th>Acción</th>
-    </tr>
 
-<tr>
-
-
-</tr>
-
-</table>
 </body>
-</html>
 
+</html>
