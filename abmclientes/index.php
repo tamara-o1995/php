9ecdefc31e5aa50459bb39126f846f05f498843c
tamara-o1995 <omelaniukt@gmail.com>
2021-04-15 14:40:57 -0300

    actividad realizada

--- abmclientes/index.php
+++ abmclientes/index.php
@@ -19,13 +19,49 @@ if ($_POST) {
     $telefono = $_POST["txtTelefono"];
     $correo = $_POST["txtCorreo"];
 
-    $aClientes[] = array(
-        "dni" => $dni,
-        "nombre" => $nombre,
-        "telefono" => $telefono,
-        "correo" => $correo,
+    if ($_FILES["archivo"]["error"] === UPLOAD_ERR_OK) {
+        $nombre = date("Ymdhmsi") . rand(1000, 5000);
+        $archivo_tmp = $_FILES["archivo"]["tmp_name"];
+        $nombreArchivo = $_FILES["archivo"]["name"];
+        $extension = pathinfo($nombreArchivo, PATHINFO_EXTENSION);
+        $nuevoNombre = "$nombre.$extension";
+        move_uploaded_file($archivo_tmp, "images/" . $nuevoNombre);
+    }
 
-    );
+
+    if ($id != "") {
+        if($_FILES["archivo"]["error"] !== UPLOAD_ERR_OK) {
+            $nuevoNombre = $aClientes[$id]["imagen"];
+        }
+    
+        if($_FILES["archivo"]["error"] === UPLOAD_ERR_OK) {
+            if(file_exists("images/" . $aClientes[$id]["imagen"])){
+                unlink("images/" . $aClientes[$id]["imagen"]);
+        }
+    }
+
+
+
+        $aClientes[$id] = array(
+            "dni" => $dni,
+            "nombre" => $nombre,
+            "telefono" => $telefono,
+            "correo" => $correo,
+            "imagen" => $nuevoNombre
+
+        );
+    } else {
+
+
+        $aClientes[] = array(
+            "dni" => $dni,
+            "nombre" => $nombre,
+            "telefono" => $telefono,
+            "correo" => $correo,
+            "imagen" => $nuevoNombre
+        );
+    }
+
     $json_encode = json_encode($aClientes);
 
     file_put_contents("archivo.txt", $json_encode);
@@ -47,90 +83,87 @@ if (isset($_GET["id"]) && $_GET["id"] != "" && isset($_GET["do"]) && $_GET["do"]
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de clientes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>ABM Clientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
     <div class="container">
-        <div class="row my-5">
-            <div class="col-12 text-center">
+        <div class="row">
+            <div class="col-12 my-5 text-center">
                 <h1>Registro de clientes</h1>
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <form action="" method="POST" enctype="multipart/form-data">
-                        <div class="my-3">
-                            <label for="">DNI:
-                                <input value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["dni"] : ""; ?>" type="text" name="txtDni" id="txtDni" class="form-control">
-                            </label>
+        <div class="row">
+            <div class="col-6">
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="row">
+                        <?php if (isset($msg) && $msg != "") : ?>
+                            <div class="alert alert-success" role="alert">
+                                <?php echo $msg; ?>
+                            </div>
+                        <?php endif; ?>
+                        <div class="col-12 form-group">
+                            <label for="txtDni">DNI: *</label>
+                            <input type="text" id="txtDni" name="txtDni" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["dni"] : "" ?>">
                         </div>
-                        <div class="my-3">
-                            <label for="">Nombre:
-                                <input value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["nombre"] : ""; ?>" type="text" id="txtNombre" name="txtNombre" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["nombre"] : "" ?>
+                        <div class="col-12 form-group">
+                            <label for="txtNombre">Nombre: *</label>
+                            <input type="text" id="txtNombre" name="txtNombre" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["nombre"] : "" ?>
 ">
-                            </label>
                         </div>
-                        <div class="my-3">
-                            <label for="">Telefono:
-                                <input value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["telefono"] : ""; ?>" type="tel" name="txtTelefono" id="txtTelefono" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["telefono"] : "" ?>
+                        <div class="col-12 form-group">
+                            <label for="txtTelefono">Teléfono:</label>
+                            <input type="text" id="txtTelefono" name="txtTelefono" class="form-control" value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["telefono"] : "" ?>
 ">
-                            </label>
                         </div>
-                        <div class="my-3">
-                            <label for="">Correo:
-                                <input value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["correo"] : ""; ?>" type="email" name="txtCorreo" id="txtCorreo" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["correo"] : "" ?>
+                        <div class="col-12 form-group">
+                            <label for="txtCorreo">Correo: *</label>
+                            <input type="text" id="txtCorreo" name="txtCorreo" class="form-control" required value="<?php echo isset($aClientes[$id]) ? $aClientes[$id]["correo"] : "" ?>
 ">
-                            </label>
                         </div>
-                        <div class="my-3">
-                            Archivo adjunto: <br>
-                            <input type="file" name="archivo" id="archivo" accept=".jpg, .jpeg, .png">
+                        <div class="col-12 form-group">
+                            <label for="txtCorreo">Archivo adjunto:</label>
+                            <input type="file" id="archivo" name="archivo" class="form-control-file" accept=".jpg, .jpeg, .png">
+                            <small class="d-block">Archivos admitidos: .jpg, .jpeg, .png</small>
                         </div>
-                        <div class="my-3">
-                            <button type="submit" class="btn btn-danger">Guardar</button>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 mt-3">
+                            <button type="submit" id="btnGuardar" name="btnGuardar" class="btn btn-primary">Guardar</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-6">
+                <table class="table table-hover border">
+                    <tr>
+                        <th>Imagen</th>
+                        <th>DNI</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Acciones</th>
+                    </tr>
+                    <?php
 
-                <div class="col-sm-6">
-                    <table class="table table-hover border">
-                        <tbody>
-                            <tr>
-                                <th>Imagen</th>
-                                <th>DNI</th>
-                                <th>Nombre</th>
-                                <th>Correo</th>
-                                <th>Acciones</th>
-                            </tr>
-                            <?php
-                            foreach ($aClientes as $indice => $cliente) : ?>
-                                <tr>
-                                    <td></td>
-                                    <td><?php echo $cliente["dni"]; ?></td>
-                                    <td><?php echo $cliente["nombre"]; ?></td>
-                                    <td><?php echo $cliente["correo"]; ?></td>
-                                    <td style="width: 110px;">
-                                        <a href="index.php?id=<?php echo $indice; ?>"><i class="fas fa-edit"></i></a>
-                                        <a class="p-1 icono-rojo" href="index.php?id=<?php echo $indice; ?>&do=eliminar"><i class="fas fa-trash-alt"></i></a>
-
-                                    </td>
-
-                                </tr>
-                            <?php endforeach;
-                            ?>
-                        </tbody>
-                    </table>
-                    <a href="index.php"><i class="fas fa-plus"></i></a>
-                </div>
+                    foreach ($aClientes as $pos => $cliente) : ?>
+                        <tr>
+                            <td><img class="img-thumbnail" src="images/<?php echo $cliente["imagen"]; ?>" alt="" srcset=""></td>
+                            <td><?php echo $cliente["dni"]; ?></td>
+                            <td><?php echo $cliente["nombre"]; ?></td>
+                            <td><?php echo $cliente["correo"]; ?></td>
+                            <td style="width: 110px;">
+                                <a href="index.php?id=<?php echo $pos; ?>"><i class="fas fa-edit"></i></a>
+                                <a href="index.php?id=<?php echo $pos; ?>&do=eliminar"><i class="fas fa-trash-alt"></i></a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+                <a href="index.php"><i class="fas fa-plus"></i></a>
             </div>
         </div>
     </div>
